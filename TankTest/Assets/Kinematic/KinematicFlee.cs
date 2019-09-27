980cdf3b7851f40bab673143ed17fc4ddea5a8f7
Gerard Clotet Rodriguez <36155703+GerardClotet@users.noreply.github.com>
2019-09-27 12:51:03 +0200

    test

---
+++ TankTest/Assets/Kinematic/KinematicFlee.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicFlee : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // TODO 6: To create flee just switch the direction to go
+
+        //Vector3 a = move.target.transform.position - this.transform.position;
+
+        //move.SetMovementVelocity(-a.normalized);
+
+        Vector3 a = move.target.transform.position - this.transform.position;
+        Vector3 a2 = a.normalized * move.max_mov_velocity;
+        move.SetMovementVelocity(-a2);
+    }
+}
---
+++ TankTest/Assets/Kinematic/KinematicSeek.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicSeek : MonoBehaviour {
+
+	Move move;
+    public float velocity;
+    public GameObject go;
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // TODO 1: Set movement velocity to max speed in the direction of the target
+        //   move.SetMovementVelocity(move.target.transform.position);
+
+        Vector3 a = move.target.transform.position -this.transform.position;
+        Vector3 a2 = a.normalized * move.max_mov_velocity;
+        move.SetMovementVelocity(a2);
+
+
+
+     //   move.mov_velocity = a;
+        //move.mov_velocity = 
+        // Remember to enable this component in the inspector
+    }
+}
